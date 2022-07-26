e9ab2ea978e46ce44856788a573c492638f8a3f7
JonasDamasceno <jonasmichael2021@outlook.com>
2022-07-26 17:39:52 -0300

    fixação 5, 6 e 7

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons./src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons./src/App.js
@@ -2,21 +2,50 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
+  constructor() {
+    super();  // Chamada para o construtor da classe pai
+
+    this.state = { 
+      countBotton1: 0,
+      countBotton2: 0,
+      countBotton3: 0,
+    }
+   
+
+    this.handleClick = this.handleClick.bind(this); 
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
   }
   handleClick() {
-    // Essa chamada ao `this` retorna `undefined`? !
-    console.log(this)
-    console.log('Clicou')
+    this.setState(( estadoAnterior, _props) => {
+      return {
+        countBotton1: estadoAnterior.countBotton1 + 1,
+      } // Retorna o novo estado
+    })   
+  }
+  handleClick2() {
+    this.setState(( estadoAnterior, _props) => {
+      return {
+        countBotton2: estadoAnterior.countBotton2 + 1,
+      } // Retorna o novo estado
+    })
+  }
+  handleClick3() {
+    this.setState(( estadoAnterior, _props) => {
+      return {
+        countBotton3: estadoAnterior.countBotton3 + 1,
+      } // Retorna o novo estado
+    })
   }
 
   render() {
-    // Já essa chamada ao `this`, feita de dentro da função `render`, retorna o objeto que esperamos
-    console.log(this)
-    return <button onClick={this.handleClick}>Meu botão</button>
+    return (
+      <div className="App">
+        <button onClick={this.handleClick}>{this.state.countBotton1}</button>
+        <button onClick={this.handleClick2}>{this.state.countBotton2}</button>
+        <button onClick={this.handleClick3}>{this.state.countBotton3}</button>
+      </div>
+    );
   }
 }
-
 export default App;
