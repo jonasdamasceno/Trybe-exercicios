a51e69214ee7a39cbedbb16feb0d542271514933
JonasDamasceno <jonasmichael2021@outlook.com>
2022-07-26 18:13:12 -0300

    exercicio de fixação 8

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons./src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons./src/App.js
@@ -15,37 +15,56 @@ class App extends React.Component {
     this.handleClick = this.handleClick.bind(this); 
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
+    this.collorButton = this.collorButton.bind(this);
   }
   handleClick() {
-    this.setState(( estadoAnterior, _props) => {
-      return {
-        countBotton1: estadoAnterior.countBotton1 + 1,
-      } // Retorna o novo estado
-    })   
+    this.setState((countBotton1) => ({
+      countBotton1: countBotton1.countBotton1 + 1,
+      }), () => {
+        console.log(`Botton 1: ${this.collorButton(this.state.countBotton1)}`);
+      });
+  
   }
   handleClick2() {
-    this.setState(( estadoAnterior, _props) => {
-      return {
-        countBotton2: estadoAnterior.countBotton2 + 1,
+    this.setState(( countBotton2) => ({
+        countBotton2: countBotton2.countBotton2 + 1,
+      }), () => {
+        console.log(`Botton 2: ${this.collorButton(this.state.countBotton2)}`);
       } // Retorna o novo estado
-    })
+      );
   }
   handleClick3() {
-    this.setState(( estadoAnterior, _props) => {
-      return {
-        countBotton3: estadoAnterior.countBotton3 + 1,
+    this.setState(( countBotton3 ) => ({
+        countBotton3: countBotton3.countBotton3 + 1,
+      }), () => {
+        console.log(`Botton 3: ${this.collorButton(this.state.countBotton3)}`);
       } // Retorna o novo estado
-    })
+      );
   }
+ 
+  collorButton(number) {
+    return number % 2 === 0 ? 'green' : 'white';
 
+  }
   render() {
+  const { countBotton1, countBotton2, countBotton3 } = this.state;
     return (
       <div className="App">
-        <button onClick={this.handleClick}>{this.state.countBotton1}</button>
-        <button onClick={this.handleClick2}>{this.state.countBotton2}</button>
-        <button onClick={this.handleClick3}>{this.state.countBotton3}</button>
+        <button onClick={this.handleClick}
+          style={{backgroundColor: this.collorButton(this.handleClick)}}>
+          botao 1 | count = {countBotton1}
+          </button>
+        
+        <button onClick={this.handleClick2}
+         style={{backgroundColor: this.collorButton(this.handleClick2)}}>
+          botao 2 | count = {countBotton2}</button>
+        <button onClick={this.handleClick3}
+          style={{backgroundColor:this.collorButton(this.handleClick3)}}>
+          botao 3 | count = {countBotton3}
+          </button>
       </div>
     );
   }
+  
 }
 export default App;
