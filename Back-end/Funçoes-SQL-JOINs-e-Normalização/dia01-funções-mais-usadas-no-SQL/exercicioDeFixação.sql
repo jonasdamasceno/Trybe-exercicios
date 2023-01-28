843132979cc52b55530075f63b0535d73427a3c4
jonasdamasceno <jonasmichael2021@outlook.com>
2023-01-27 23:19:06 -0300

    nova tentativa

---
+++ Back-end/Funçoes-SQL-JOINs-e-Normalização/dia01-funções-mais-usadas-no-SQL/exercicioDeFixação.sql
@@ -0,0 +1,10 @@
+SELECT IF(220 MOD 12 = 0, "Não", CONCAT("SIM, ", 220 MOD 12));
+
+#querys dos exercicios de fixação do dia sobre funçẽos de arredondamento matematico
+SELECT ROUND(15 + (RAND() * 5));
+
+SELECT ROUND(15.7515791, 5);
+
+SELECT FLOOR(39.494);
+
+SELECT CEIL(85.234) AS valor_aproximado
