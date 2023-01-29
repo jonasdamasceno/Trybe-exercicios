f18ae228b3df2608d050a865ba4b7c7c7989508c
jonasdamasceno <jonasmichael2021@outlook.com>
2023-01-29 14:05:04 -0300

    exercicios sobre 'Utilizando as funções de agregação AVG, MIN, MAX, SUM e COUNT'

---
+++ Back-end/Funçoes-SQL-JOINs-e-Normalização/dia01-funções-mais-usadas-no-SQL/exercicios sobre funçoes no sql.sql
@@ -0,0 +1,17 @@
+#Utilizando as funções de agregação AVG, MIN, MAX, SUM e COUNT.
+
+#A média de duração dos filmes e dê o nome da coluna de ‘Média de Duração’;
+SELECT AVG(length) AS "Média de Duração" 
+FROM sakila.film;
+
+#A duração mínima dos filmes como ‘Duração Mínima’;
+SELECT MIN(length) AS "Duração Minima"
+FROM sakila.film;
+
+#A soma de todas as durações como ‘Tempo de Exibição Total’;
+SELECT MAX(length) AS "Tempo de Exibição Total"
+FROM sakila.film;
+
+#E, finalmente, a quantidade total de filmes cadastrados na tabela sakila.film como ‘Filmes Registrados’.
+SELECT COUNT() AS "Filmes Registrados"
+FROM sakila.film;
