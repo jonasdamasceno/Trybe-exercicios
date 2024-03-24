2f31aa4c6c4fb707085fc241b0ceb26ea2f2b669
jdamasceno <104797876+jonasdamasceno@users.noreply.github.com>
2024-03-24 02:20:41 -0300

    Update exercicio2.py
    fixed

--- CIENCIAS DA COMPUTAÇÃO/Introdução à Python/Entrada e saida de dados com testes/exercicio2.py
+++ CIENCIAS DA COMPUTAÇÃO/Introdução à Python/Entrada e saida de dados com testes/exercicio2.py
@@ -7,37 +7,42 @@
 import random
 
 WORDS = [
-    "excretor",
-    "pronador",
-    "halógeno",
-    "ocultado",
-    "traquin"
+    "cat",
+    "elephant",
+    "dog",
+    "monkey",
+    "duck",
+    "chameleon",
+    "bear",
+    "moose",
+    "rooster",
 ]
+MAX_ATTEMPTS = 3
 
 
-def adivinha_palavra(word):
-    random_word = random.choice(word)
-    scramble_word = "".join(random.sample(random_word, len(random_word)))
-    return random_word, scramble_word
+def draw_secret_word(words):
+    secret_word = random.choice(words)
+    scrambled_word = "".join(random.sample(secret_word, len(secret_word)))
+    return secret_word, scrambled_word
 
 
-def tentativa_usuario():
+def collect_guesses():
     guesses = []
-    for attempt in range(3):
-        guess = input("digite aqui a sua palavra")
+    for attempt in range(MAX_ATTEMPTS):
+        guess = input("Guess the word: ")
         guesses.append(guess)
     return guesses
 
 
-def verifica_resultado(secret_word, guesses):
+def check_game_result(secret_word, guesses):
     if secret_word in guesses:
-        print(f"você venceu a palavra é {secret_word}")
+        print(f"You win: {secret_word}")
     else:
-        print(f"você perdeu a palavra é: {secret_word}")
+        print(f"You lose: {secret_word}")
 
 
-if __name__ == "_main_":
-    secret_word, scramble_word = draw_secret_word(WORDS)
-    print(f"Scramble_word is: {secret_word}")
+if __name__ == "__main__":
+    secret_word, scrambled_word = draw_secret_word(WORDS)
+    print(f"Scrambled word is {scrambled_word}")
     guesses = collect_guesses()
     check_game_result(secret_word, guesses)
